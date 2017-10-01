e2b3df06a0d5b8c146cb05707d453604c0e50ba3
͡° ͜ʖ ͡° <artgreg@outlook.fr>
2017-10-01 10:46:42 +0200

    Update sf3-app-osx.sh

--- sf3-app-osx.sh
+++ sf3-app-osx.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
-#chmod a+x /usr/local/bin/symfony
+curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
+chmod a+x /usr/local/bin/symfony
 symfony new sf3 3.3
 chmod -R 777 sf3/var/{cache,logs,sessions}
 cp parameters.yml sf3/app/config/parameters.yml
