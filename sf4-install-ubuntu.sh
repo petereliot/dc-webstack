0e5187ab4ff9bb22a0f02b58cd98bf6ffbcce3a0
͡° ͜ʖ ͡° <artgreg@outlook.fr>
2017-12-16 14:17:39 +0100

    added env file copy in bash

--- sf4-install-ubuntu.sh
+++ sf4-install-ubuntu.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin/ --filename=composer
 composer create-project symfony/skeleton sf4
+cp .env ./sf4/.env
