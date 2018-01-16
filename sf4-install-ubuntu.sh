dd2a1c9793db8c0ce645dd8176ed9b92fe4c717c
͡° ͜ʖ ͡° <artgreg@outlook.fr>
2018-01-16 15:08:07 +0100

    - added doctrine + web profiler - @todo tests for web profiler

--- sf4-install-ubuntu.sh
+++ sf4-install-ubuntu.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin/ --filename=composer
 composer create-project symfony/skeleton sf4
+composer require doctrine maker
 cp .env ./sf4/.env
+cp web_profiler.yaml ./sf4/config/packages/dev/web_profiler.yaml
